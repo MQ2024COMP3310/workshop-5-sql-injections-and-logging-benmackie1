a31c6d1829c799aef1f86ad6aeb021b6044ae345
Ben Mackie <mackie.benjamin@gmail.com>
2024-03-24 15:22:30 +1100

    Update SQLiteConnectionManager.java

--- app/src/main/java/workshop05code/SQLiteConnectionManager.java
+++ app/src/main/java/workshop05code/SQLiteConnectionManager.java
@@ -148,7 +148,8 @@ public class SQLiteConnectionManager {
         String sql = "SELECT count(id) as total FROM validWords WHERE word like'" + guess + "';";
 
         try (Connection conn = DriverManager.getConnection(databaseURL);
-                PreparedStatement stmt = conn.prepareStatement(sql)) {
+                PreparedStatement stmt = conn.prepareStatement(sql))
+                stmt.setString(1, sql){
 
             ResultSet resultRows = stmt.executeQuery();
             if (resultRows.next()) {
